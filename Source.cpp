206965883d30e808a1a3e04aa6b918a19d2323c1
Терентий <ya.terenti.ak@yandex.ru>
2020-02-02 21:02:34 +0700

    Endeavour визуализация

--- Source.cpp
+++ Source.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <cmath>
 
 
@@ -17,10 +18,11 @@ using namespace std;
 
 
 
-void type0(const matrix & _M_, const HKL hkl, const double ksi, const double psi);
-void type1(const matrix& _M_, const HKL hkl, const double ksi, const double psi);
-void type2(const matrix& _M_, const HKL hkl, const double ksi, const double psi);
-void type_def(const matrix& _M_, const HKL hkl, const double ksi, const double psi);
+void type0(const matrix & _M_, const HKL hkl, const double ksi, const double psi, ostream & endeavour);
+void type1(const matrix& _M_, const HKL hkl, const double ksi, const double psi, ostream & endeavour);
+void type2(const matrix& _M_, const HKL hkl, const double ksi, const double psi, ostream & endeavour);
+void type_def(const matrix& _M_, const HKL hkl, const double ksi, const double psi, ostream & endeavour);
+
 
 
 //MAIN
@@ -67,34 +69,61 @@ int main(int argn, char* argv[]) {
 
 	cout << "-h- -k- -l- -ksi- psi-  -2theta- [   -omega-   -phi-    -chi-    ]\n\n";
 	Gonio g(_M_);
-	//ofstream out("output 1,0; 2,0; 3,0; psi = 180; .txt");        //файл, куда выводим траекторию
 	while (!reader.eof()) {
+		ofstream endeavour;        //файл, куда выводим траекторию
+
 		switch (reader.read(hkl, ksi, psi)) {
 		case 0:
 
 			if (hkl.h == 0 && hkl.k == 0 && hkl.l == 0) break;
+			
+			{
+				stringstream strstr;
+				strstr << hkl.h << "_" << hkl.k << "_" << hkl.l << "_" << ksi << "_" << psi << "_0.xyz";
+				endeavour.open(strstr.str());
+			}
 
-			type0(_M_, hkl, ksi, psi);
+			type0(_M_, hkl, ksi, psi, endeavour);
+			endeavour.close();
 			break;
 		case 1:
 
 			if (hkl.h == 0 && hkl.k == 0 && hkl.l == 0) break;
 
-			type1(_M_, hkl, ksi, psi);
+			{
+				stringstream strstr;
+				strstr << hkl.h << "_" << hkl.k << "_" << hkl.l << "_" << ksi << "_" << psi << "_1.xyz";
+				endeavour.open(strstr.str());
+			}
+
+			type1(_M_, hkl, ksi, psi, endeavour);
+			endeavour.close();
 			break;
 
 		case 2:
 
 			if (hkl.h == 0 && hkl.k == 0 && hkl.l == 0) break;
 
-			type2(_M_, hkl, ksi, psi);
+			{
+				stringstream strstr;
+				strstr << hkl.h << "_" << hkl.k << "_" << hkl.l << "_" << ksi << "_" << psi << "_2.xyz";
+				endeavour.open(strstr.str());
+			}
+
+			type2(_M_, hkl, ksi, psi, endeavour);
 			break;
 
 		default:
 
 			if (hkl.h == 0 && hkl.k == 0 && hkl.l == 0) break;
 
-			type_def(_M_, hkl, ksi, psi);
+			{
+				stringstream strstr;
+				strstr << hkl.h << "_" << hkl.k << "_" << hkl.l << "_" << ksi << "_" << psi << "_def.xyz";
+				endeavour.open(strstr.str());
+			}
+
+			type_def(_M_, hkl, ksi, psi, endeavour);
 			
 			break;
 
@@ -111,61 +140,126 @@ int main(int argn, char* argv[]) {
 
 
 
-void type0(const matrix& _M_, const HKL hkl, const double ksi, const double psi)
+void type0(const matrix& _M_, const HKL hkl, const double ksi, const double psi, ostream & endeavour)
 {
 	Gonio g(_M_);
 	cout << hkl.h << " " << hkl.k << " " << hkl.l << " " << "-ksi-" << " " << psi << "  " << 2 * rad_to_degrees(Diff::th(_M_, hkl)) << endl << endl;
-
+	//сначала записываем в файл число точек
+	endeavour << 2*(int)(360 / ksi)+8 << endl;
+	//комментарий для файла
+	endeavour << "ksi trajectory with " << ksi << " deg step" << " !! manualy cropped, in 10th degrees !!" << endl;
+	endeavour << "U -18 -18 -10\nW 18 - 18 - 10\nF - 18 18 - 10\nH - 18 - 18 10\nB - 18 18 10\nB 18 - 18 10\nB 18 18 - 10\nB 18 18 10\n";
 	for (double k = 0; k <= 360; k += ksi) {
-
+		char flag = 'C';
 		for (auto d : g.diff_rotation(degrees_to_rades(psi), degrees_to_rades(k), hkl)) {
+			if (flag == 'C') {
+				endeavour << "C ";
+				flag = 'O';
+			}
+			else {
+				endeavour << "O ";
+				flag = 'C';
+			}
 			cout << hkl.h << " " << hkl.k << " " << hkl.l << " " << k << " " << psi << "  " << 2 * rad_to_degrees(Diff::th(_M_, hkl))
 				<< "		[   " << rad_to_degrees(d.omega) << "  " << rad_to_degrees(d.phi) << "  " << rad_to_degrees(d.chi) << "	]	\n";
+			endeavour << rad_to_degrees(d.omega)/10 << "  " << rad_to_degrees(d.phi)/10 << "  " << rad_to_degrees(d.chi)/10 << endl;
+
 		}
 		cout << endl;
 	}
 }
 
-void type1(const matrix& _M_, const HKL hkl, const double ksi, const double psi)
+void type1(const matrix& _M_, const HKL hkl, const double ksi, const double psi, ostream & endeavour)
 {
 	Gonio g(_M_);
 	cout << hkl.h << " " << hkl.k << " " << hkl.l << " " << ksi << " " << "-psi-" << "  " << 2 * rad_to_degrees(Diff::th(_M_, hkl)) << endl << endl;
-
+	//сначала записываем в файл число точек
+	endeavour << 2 * (int)(360 / psi)+8 << endl;
+	//комментарий для файла
+	endeavour << "psi trajectory with " << psi << " deg step" << " !! manualy cropped, in 10th degrees !!" << endl;
+	endeavour << "U -18 -18 -10\nW 18 - 18 - 10\nF - 18 18 - 10\nH - 18 - 18 10\nB - 18 18 10\nB 18 - 18 10\nB 18 18 - 10\nB 18 18 10\n";
 	for (double p = 0; p <= 360; p += psi) {
-
+		char flag = 'C';
 		for (auto d : g.diff_rotation(degrees_to_rades(p), degrees_to_rades(ksi), hkl)) {
+			if (flag == 'C') {
+				endeavour << "C ";
+				flag = 'O';
+			}
+			else {
+				endeavour << "O ";
+				flag = 'C';
+			}
 			cout << hkl.h << " " << hkl.k << " " << hkl.l << " " << ksi << " " << p << "  " << 2 * rad_to_degrees(Diff::th(_M_, hkl))
 				<< "		[   " << rad_to_degrees(d.omega) << "  " << rad_to_degrees(d.phi) << "  " << rad_to_degrees(d.chi) << "	]	\n";
+
+			endeavour << rad_to_degrees(d.omega)/10 << "  " << rad_to_degrees(d.phi)/10 << "  " << rad_to_degrees(d.chi)/10 << endl;
+
 		}
 		cout << endl;
 	}
 }
 
-void type2(const matrix& _M_, const HKL hkl, const double ksi, const double psi)
+void type2(const matrix& _M_, const HKL hkl, const double ksi, const double psi, ostream & endeavour)
 {
 	Gonio g(_M_);
 	cout << hkl.h << " " << hkl.k << " " << hkl.l << " " << "-ksi-" << " " << "-psi-" << "  " << 2 * rad_to_degrees(Diff::th(_M_, hkl)) << endl << endl;
 
+	//сначала записываем в файл число точек
+	endeavour << 2 * (int)(360 / psi) * 2 * (int)(360/ksi) +8 << endl;
+	//комментарий для файла
+	endeavour << "ksi&psi trajectory with " << ksi<<" & "<< psi << " deg step" << " !! manualy cropped, in 10th degrees !!" << endl;
+	endeavour << "U -18 -18 -10\nW 18 - 18 - 10\nF - 18 18 - 10\nH - 18 - 18 10\nB - 18 18 10\nB 18 - 18 10\nB 18 18 - 10\nB 18 18 10\n";
 	for (double k = 0; k <= 360; k += ksi) {
 		for (double p = 0; p <= 360; p += psi) {
-
+			char flag = 'C';
 			for (auto d : g.diff_rotation(degrees_to_rades(p), degrees_to_rades(k), hkl)) {
+
+				if (flag == 'C') {
+					endeavour << "C ";
+					flag = 'O';
+				}
+				else {
+					endeavour << "O ";
+					flag = 'C';
+				}
+
 				cout << hkl.h << " " << hkl.k << " " << hkl.l << " " << k << " " << p << "  " << 2 * rad_to_degrees(Diff::th(_M_, hkl))
 					<< "		[   " << rad_to_degrees(d.omega) << "  " << rad_to_degrees(d.phi) << "  " << rad_to_degrees(d.chi) << "	]	\n";
+
+				endeavour << rad_to_degrees(d.omega)/10 << "  " << rad_to_degrees(d.phi)/10 << "  " << rad_to_degrees(d.chi)/10 << endl;
+			
 			}
 			cout << endl;
 		}
 	}
 }
 
-void type_def(const matrix& _M_, const HKL hkl, const double ksi, const double psi)
+void type_def(const matrix& _M_, const HKL hkl, const double ksi, const double psi, ostream & endeavour)
 {
 	Gonio g(_M_);
 	cout << hkl.h << " " << hkl.k << " " << hkl.l << " " << "-ksi-" << " " << psi << "  " << 2 * rad_to_degrees(Diff::th(_M_, hkl)) << endl << endl;
 
+	//сначала записываем в файл число точек
+	endeavour << 2+8 << endl;
+	//комментарий для файла
+	endeavour << "position with " << ksi << ' & ' << psi << " !! manualy cropped, in 10th degrees !!" << endl;
+	endeavour << "U -18 -18 -10\nW 18 - 18 - 10\nF - 18 18 - 10\nH - 18 - 18 10\nB - 18 18 10\nB 18 - 18 10\nB 18 18 - 10\nB 18 18 10\n";
+	char flag = 'C';
 	for (auto d : g.diff_rotation(degrees_to_rades(psi), degrees_to_rades(ksi), hkl)) {
+
+		if (flag == 'C') {
+			endeavour << "C ";
+			flag = 'O';
+		}
+		else {
+			endeavour << "O ";
+			flag = 'C';
+		}
+
 		cout << hkl.h << " " << hkl.k << " " << hkl.l << " " << ksi << " " << psi << "  " << 2 * rad_to_degrees(Diff::th(_M_, hkl))
 			<< "		[   " << rad_to_degrees(d.omega) << "  " << rad_to_degrees(d.phi) << "  " << rad_to_degrees(d.chi) << "	]	\n";
+
+		endeavour << rad_to_degrees(d.omega)/10 << "  " << rad_to_degrees(d.phi)/10 << "  " << rad_to_degrees(d.chi)/10 << endl;
 	}
 	cout << endl;
 }
